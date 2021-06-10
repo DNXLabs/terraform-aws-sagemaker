0928e339ad3d62eb729710074742b16e955f9c1f
maicon.rocha@dnx.solutions <maicon.rocha@dnx.solutions>
2021-06-10 07:48:14 +0000

    fix format and add user profile

--- sagemaker_user_profile.tf
+++ sagemaker_user_profile.tf
@@ -1,7 +1,7 @@
 resource "aws_sagemaker_user_profile" "sagemaker_user_profile" {
 
-  user_profile_name         = "datascientist"
-  domain_id                 = aws_sagemaker_domain.domain.id
+  user_profile_name = "datascientist"
+  domain_id         = aws_sagemaker_domain.domain.id
   #single_sign_on_user_value = var.sagemaker_user_profile_single_sign_on_user_value
 
   #single_sign_on_user_identifier = var.sagemaker_user_profile_single_sign_on_user_identifier
@@ -11,4 +11,4 @@ resource "aws_sagemaker_user_profile" "sagemaker_user_profile" {
 
     security_groups = [aws_security_group.sagemakerstudio.id]
   }
-  }
+}
